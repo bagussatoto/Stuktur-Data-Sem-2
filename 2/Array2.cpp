fe634eb878651c44d205bfb1756eb806e046fd8d
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2022-07-07 14:02:25 +0700

    Create Array2.cpp

---
+++ 2/Array2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string my_str = "Amikom Yogyakarta";
+	
+	// menghintung panjang karakter
+	cout << "string      : " << my_str << endl;
+	cout << "jml karakter: " << my_str.length() << endl;
+	
+	return 0;
+}
